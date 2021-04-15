9fc1c1e153678fbae95629dc023079f8884caf67
Toren Danino <daninotoren@gmail.com>
2021-04-15 19:41:05 +0300

    final commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+// connect to DB (DATABASE_URL provided by environment properties)
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -10,10 +11,16 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
+// use express
 app.use(express.json());
 
+// add weather routes
 const weatherRouter = require('./routes/weather');
 app.use('/weather', weatherRouter);
+
+app.get('/', (req, res) => {
+  res.send('Weather API');
+});
 
 const port = process.env.port || 3000;
 app.listen(port, () => console.log('Server Started'));
